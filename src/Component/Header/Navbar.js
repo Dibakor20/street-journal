49f472216dbeaf57eaef08c157ec8e92c3d40f49
Debakor10 <debakor10@gmail.com>
2021-06-16 21:34:49 +0600

    navbar fix

--- src/Component/Header/Navbar.js
+++ src/Component/Header/Navbar.js
@@ -3,14 +3,15 @@ import './Navbar.css'
 
 const Navbar = () => {
     return (
-        <>
-           <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ml-auto">
-                <li class="nav-item ">
+        <> 
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mx-auto">
+            <li class="nav-item ">
                     <a class="nav-link" href="#">Home </a>
                 </li>
                 <li class="nav-item">
@@ -49,12 +50,14 @@ const Navbar = () => {
                 <li class="nav-item">
                     <a class="nav-link" href="#">Sports</a>
                 </li>
-                 <li class="nav-item search-icon">search 
+            </ul>
+            
+
+             <a class="search-icon">search 
                  
                  <button type="button" data-toggle="modal" data-target="#myModal" class="btn btn-default navbar-btn">
                  <i class="far fa-search ml-3 mr-2"></i>
                 </button>
-
 
                 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
               <div class="modal-dialog" role="document">
@@ -73,16 +76,12 @@ const Navbar = () => {
                   </div>
                   </div>
                   </div>
-
-
                 </div>
-           
-            
+                 </a> 
+        </div>
+        </nav>
 
-                 </li> 
-                </ul>
-            </div>
-            </nav> 
+
         </>
     );
 };
