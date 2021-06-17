92ce3bce3cc102e60c5d70510ec4a34633009c20
Debakor10 <debakor10@gmail.com>
2021-06-17 19:09:27 +0600

    chart fix

--- src/Component/NewsStatistics/NewsChart.js
+++ src/Component/NewsStatistics/NewsChart.js
@@ -1,38 +1,75 @@
-import React from 'react';
-import { Line } from 'react-chartjs-2';
+import React from 'react'
+import './NewsChart.css'
+import {
+  ResponsiveContainer,
+  AreaChart,
+  XAxis,
+  YAxis,
+  Area,
+  Tooltip,
+  CartesianGrid,
+} from "recharts";
+import { format, parseISO, subDays } from "date-fns";
 
-const data = {
-  labels: ['1', '2', '3', '4', '5', '6'],
-  datasets: [
-    {
-      label: 'US',
-      data: [12, 19, 3, 5, 2, 3],
-      fill: false,
-      backgroundColor: 'rgb(255, 99, 132)',
-      borderColor: 'rgba(255, 99, 132, 0.2)',
-    },
-  ],
-};
+const data = [];
+for (let num = 30; num >= 0; num--) {
+  data.push({
+    date: subDays(new Date(), num).toISOString().substr(0, 10),
+    value: 1 + Math.random(),
+  });
+}
 
-const options = {
-  scales: {
-    yAxes: [
-      {
-        ticks: {
-          beginAtZero: true,
-        },
-      },
-    ],
-  },
-};
-
-
-const NewsChart = () => {
+export default function NewsChart() {
   return (
-    <>
-      <Line data={data} options={options} />
-    </>
-  );
-};
+    <ResponsiveContainer width="100%" height={150}>
+      <AreaChart data={data}>
+        <defs>
+          <linearGradient id="color" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="0%" stopColor="#2451B7" stopOpacity={0.4} />
+            <stop offset="75%" stopColor="#2451B7" stopOpacity={0.05} />
+          </linearGradient>
+        </defs>
 
-export default NewsChart;
+        <Area dataKey="value" stroke="#2451B7" fill="url(#color)" />
+
+        <XAxis
+          dataKey="date"
+          axisLine={false}
+          tickLine={false}
+          tickFormatter={(str) => {
+            const date = parseISO(str);
+            if (date.getDate() % 7 === 0) {
+              return format(date, "MMM, d");
+            }
+            return "";
+          }}
+        />
+
+        <YAxis
+          datakey="value"
+          axisLine={false}
+          tickLine={false}
+          tickCount={8}
+          tickFormatter={(number) => `${number.toFixed(1)}`}
+        />
+
+        <Tooltip content={<CustomTooltip />} />
+
+        <CartesianGrid opacity={0.1} vertical={false} />
+      </AreaChart>
+    </ResponsiveContainer>
+  );
+}
+
+function CustomTooltip({ active, payload, label }) {
+  if (active) {
+    return (
+      <div className="tooltip">
+        <h4>{format(parseISO(label), "eeee, d MMM, yyyy")}</h4>
+        <p>{payload[0].value.toFixed(2)} CAD</p>
+      </div>
+    );
+  }
+  return null;
+}
+
